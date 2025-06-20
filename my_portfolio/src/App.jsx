67f47ce0483573155b689df542d77c66eeda98b1
Vedant2802 <avedant981@gmail.com>
2025-06-21 02:55:52 +0530

    added the spinner svg and removed direct dom manipulation

--- my_portfolio/src/App.jsx
+++ my_portfolio/src/App.jsx
@@ -2,30 +2,37 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import { Header } from "./components/Header";
 import { useEffect, useState } from "react";
+import loader from "./assets/loader.svg";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  let intialLoadingValue = 0;
+  const [loadingPercentage, setLoadingPercentage] = useState(0);
+
   useEffect(() => {
     const interval = setInterval(() => {
-      intialLoadingValue += 1;
-      if (intialLoadingValue === 100) {
-        setLoading(false);
-        clearInterval(interval);
-      }
-      document.getElementById(
-        "loading-spinner"
-      ).textContent = `${intialLoadingValue}%`;
+      setLoadingPercentage((prev) => {
+        const newValue = prev + 1;
+        if (newValue === 100) {
+          setLoading(false);
+          clearInterval(interval);
+        }
+        return newValue;
+      });
     }, 20);
     return () => clearInterval(interval);
   }, []);
+
   return (
     <div>
       {loading && (
-        <div
-          className="flex justify-center items-center h-screen text-4xl font-bold"
-          id="loading-spinner"
-        ></div>
+        <div className="flex justify-center items-center h-screen text-4xl font-bold gap-3">
+          <img
+            src={loader}
+            alt="loader"
+            className="w-[250px] h-[250px] animate-spin"
+          />
+          <span className="fixed ml-5">{loadingPercentage}%</span>
+        </div>
       )}
       {!loading && (
         <>
